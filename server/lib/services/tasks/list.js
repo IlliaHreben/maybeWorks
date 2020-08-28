3a7fa218f8db0c8643c542c623b04f88d6469e73
Illia Hreben <muomieg@gmail.com>
2020-08-28 23:24:12 +0300

    query hadler to tasks

--- server/lib/services/tasks/list.js
+++ server/lib/services/tasks/list.js
@@ -4,75 +4,68 @@ const {Tasks, Users} = require('../../model')
 const formatTask = require('./format')
 
 const findProject = async filters => {
-  const {
-    page = 1,
-    pageSize = 10,
-    search = '', // name or description
-    statuses = '',
-    author = '', // name or surname
-    assignee = '', //name or surname
-    assigneeIds = null,
-    marks = null
-  } = filters
+  const { page, pageSize } = filters
+  
   const offset = page * pageSize - pageSize
   const limit = page * pageSize
-  const formatedStatuses = statuses.split(',')
-  const formatedAssigneeIds = assigneeIds.split(',')
-  const formatedMarks = marks.split(',')
+
+  const tasksWhere = {}
+  if (filters.search) {
+    tasksWhere[Op.and] = {
+      [Op.or]: {
+        name: { [Op.startsWith]: filters.search },
+        description: { [Op.startsWith]: filters.search }
+      }
+    }
+  }
+  if (filters.statuses) {
+    const formatedStatuses = { [Op.in]: filters.statuses.split(',') }
+
+    tasksWhere[Op.and]
+    ? tasksWhere[Op.and].status = formatedStatuses
+    : tasksWhere[Op.and] = { status: formatedStatuses }
+  }
+  if (filters.marks) {
+    const formatedMarks = { [Op.in]: filters.marks.split(',') }
+
+    tasksWhere[Op.and]
+    ? tasksWhere[Op.and].mark = formatedMarks
+    : tasksWhere[Op.and] = { mark: formatedMarks }
+  }
+
+  const usersWhere = {}
+  if (filters.author) {
+    usersWhere[Op.or] = {
+      name: { [Op.startsWith]: filters.author },
+      surname: { [Op.startsWith]: filters.author }
+    }
+  }
+
+  const assigneeWhere = {}
+  if (filters.assignee) {
+    usersWhere[Op.or] = {
+      name: { [Op.startsWith]: filters.assignee },
+      surname: { [Op.startsWith]: filters.assignee }
+    }
+  }
+  if (filters.assigneeId) {
+    usersWhere[Op.or]
+    ? usersWhere[Op.or].id = filters.assigneeId
+    : usersWhere[Op.or] = {id: filters.assigneeId}
+  }
 
 
   const {rows, count} = await Tasks.findAndCountAll({
-    where: {
-      [Op.and]: {
-
-        [Op.or]:
-          {
-            name: {
-              [Op.startsWith]: search // like has low performance
-            },
-
-            description: {
-              [Op.startsWith]: search
-            }
-          }
-        ,
-        status: { [Op.in]: formatedStatuses },
-        mark: { [Op.in]: formatedMarks }
-      }
-    },
+    where: tasksWhere,
     offset,
     limit,
     include: [
       {
         model: Users,
-        where: {
-          [Op.or]: {
-            name: {
-              [Op.startsWith]: author
-            },
-
-            surname: {
-              [Op.startsWith]: author
-            }
-          }
-        }
+        where: usersWhere
       }, {
         model: Users,
-        where: {
-          [Op.or]: {
-            name: {
-              [Op.startsWith]: assignee
-            },
-
-            surname: {
-              [Op.startsWith]: assignee
-            },
-
-            id: {
-              [Op.in]: formatedAssigneeIds
-            }
-          }
-        }
+        where: assigneeWhere
       }
     ]
   })
