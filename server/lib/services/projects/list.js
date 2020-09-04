db06f105179a581e71317db1898341f64d6ae344
Illia Hreben <muomieg@gmail.com>
2020-09-04 17:40:45 +0300

    fixed search filters

--- server/lib/services/projects/list.js
+++ server/lib/services/projects/list.js
@@ -6,13 +6,13 @@ const formatProject = require('./format')
 const validatorRules = {
   page: 'page_number',
   pageSize: 'page_size',
-  projectSearch: [ 'string', { max_length: 100 } ],
-  statuses: [ 'task_statuses' ],
+  projectSearch: ['not_empty', 'string', { max_length: 100 } ],
+  statuses: 'task_statuses',
   minMark: [ 'not_empty', 'task_mark', {default: 0} ],
   maxMark: [ 'not_empty', 'task_mark', {default: 5} ],
   author: [ 'not_empty', 'string', { min_length: 2 } ],
   participantSearch: [ 'not_empty', 'string', { min_length: 2 } ],
-  participantId: [ 'not_empty', 'positive_integer']
+  participantId: 'positive_integer'
 }
 
 const execute = async filters => {
@@ -20,9 +20,12 @@ const execute = async filters => {
 
   const offset = page * pageSize - pageSize
   const limit = page * pageSize
+  console.log('---------------------------------')
+  console.log(filters)
 
   const projectsWhere = {}
   if (filters.projectSearch) {
+    console.log('---------------------------------projectsWhere')
     projectsWhere[Op.and] = {
       [Op.or]: {
         name: { [Op.startsWith]: filters.projectSearch },
@@ -31,18 +34,21 @@ const execute = async filters => {
     }
   }
   if (filters.statuses) {
+    console.log('---------------------------------statuses')
     projectsWhere[Op.and]
-    ? projectsWhere[Op.and].status = filters.statuses.split(',')
-    : projectsWhere[Op.and] = { status: filters.statuses.split(',') }
+    ? projectsWhere[Op.and].status = filters.statuses
+    : projectsWhere[Op.and] = { status: filters.statuses }
   }
 
-  const tasksWhere = {}
-  if (filters.marks) {
-    tasksWhere.mark = { [Op.between]: filters.marks.split(',') }
+  const tasksWhere = {
+    mark: {
+      [Op.between]: [filters.minMark, filters.maxMark]
+    }
   }
 
   const authorWhere = {}
   if (filters.author) {
+    console.log('---------------------------------author')
     authorWhere[Op.or] = {
       name: { [Op.startsWith]: filters.author },
       surname: { [Op.startsWith]: filters.author }
@@ -51,6 +57,7 @@ const execute = async filters => {
 
   const usersWhere = {}
   if (filters.participantSearch) {
+    console.log('---------------------------------participantSearch')
     usersWhere[Op.and] = {
       [Op.or]: {
         name: { [Op.startsWith]: filters.participantSearch },
@@ -59,11 +66,12 @@ const execute = async filters => {
     }
   }
   if (filters.participantId) {
+    console.log('---------------------------------participantId')
     usersWhere[Op.and]
     ? usersWhere[Op.and].id = filters.participantId
     : usersWhere[Op.and] = { id: filters.participantId }
   }
-  console.log(usersWhere, filters)
+  console.log(projectsWhere,tasksWhere, authorWhere , usersWhere)
 
   const {rows, count} = await Projects.findAndCountAll({
     where: projectsWhere,
